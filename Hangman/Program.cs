ada1f8b6d1a0f3424c60f7e1c4eba49cda26d225
Colin Pierce <cpierce46@gmail.com>
2014-06-27 12:35:54 -0600

    made changes

---
+++ Hangman/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //keeps the window open
+            Console.ReadKey();
+        }
+        static void Hangman()
+        {
+            //ask for users name
+            Console.WriteLine("What is your name?");
+            string username = Console.ReadLine();
+            //greet user and give instructions
+            Console.WriteLine("Hello. Correctly guess the letters or word you win. Guess incorrectly and you will lose.");
+            bool playing = true;
+            while (playing == true)
+            {
+                //choose a radom word from list
+                List<string> items = ("baseball", "tennisball", "basketball");
+
+            }
+        }
+    }
+}
